c22d241ea92c08ffefa79e60b56667b9cb258888
mohfikryansyah <moh.fikryansyah@gmail.com>
2024-12-07 13:57:02 +0800

    fix audio

--- public/build/assets/Main-ClD3oKZr.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./app-ByKrDDBW.js";import i from"./Navbar-CoGDxHO3.js";import"./sheet-DKcEuGXK.js";import"./Combination-C2G9Z1IB.js";import"./utils-CU4Wmg4P.js";function p({children:t}){return r.jsxs(r.Fragment,{children:[r.jsx(i,{}),r.jsx("main",{children:t})]})}export{p as default};
